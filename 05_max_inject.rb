5a7bb51f4bf0e9244aaa594ee653ca4d9d0661e0
Zane Vandergriff <zanevande@gmail.com>
2021-04-14 21:39:30 -0400

    working on problems

--- 05_max_inject.rb
+++ 05_max_inject.rb
@@ -1,6 +1,11 @@
 # Write a method, max_inject(arr), that accepts any amount of numbers arguments and returns
 # the largest number. Solve this using the built-in inject.
 
+def max_inject(arr)
+
+    arr.inject { |acc, el| acc + el } #something isn't right, error: wrong number of arguments (given 5, expected 1)
+
+end
 
 p max_inject(1, -4, 0, 7, 5)  # => 7
 p max_inject(30, 28, 18)      # => 30
